16e69eb4ca35047d7fb95f16feee459921b20daf
ignaciosua.h <ignaciosua.h@gmail.com>
2021-01-01 20:08:09 -0600

    Promise to go!

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.promiseToGo = async function(promise) {
+    try {
+      let data = await promise
+      return {data: data, error: null}
+    } catch (error) {
+      return {data: null, error: error}
+    }
+}
