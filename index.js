1c1b57df5370bdc91b30326ba8d3f4684ac6df25
ignaciosua.h <ignaciosua.h@gmail.com>
2021-01-03 17:37:14 -0600

    Adding flag to switch between object and array on returns

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const promisetogo = async function (promise) {
-  try {
-    let data = await promise;
-    return { data: data, error: null };
-  } catch (error) {
-    return { data: null, error: error };
+const promisetogo = async function (promise, toObject) {
+  if (toObject) {
+    try {
+      let data = await promise;
+      return { data: data, error: null };
+    } catch (error) {
+      return { data: null, error: error };
+    }
+  } else {
+    try {
+      let data = await promise;
+      return [data, null];
+    } catch (error) {
+      return [null, error];
+    }
   }
 };
 
